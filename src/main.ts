b79a77a61d47de1a3b75d7d8cf74deef37991ef0
JaluGJ <gabriel.i.jalil@gmail.com>
2023-08-02 11:27:27 -0300

    add: added new configurations on main an app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /*  const configService = app.get(ConfigService);
+
+  const LOCAL = configService.get<string>('LOCAL');
+  const FRONTEND = configService.get<string>('FRONT_URL');
+  
+  if (LOCAL) {
+    app.enableCors({
+      origin: `${FRONTEND}`,
+      credentials: true,
+    });
+  } else {
+    app.enableCors({
+      origin: `*`,
+      credentials: true,
+    });
+  } */
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: `*`,
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +41,6 @@ async function bootstrap() {
   app.enableCors({
     origin: '*', // Reemplaza esto con el origen de tu cliente
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
