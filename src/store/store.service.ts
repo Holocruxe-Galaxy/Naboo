07789607f2ce7bd0b9d0c3386a343a2af66c6574
JaluGJ <gabriel.i.jalil@gmail.com>
2023-08-09 18:00:36 -0300

    add(store): Added functions and schema to save it

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -2,20 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Store } from './schema';
 import { Model } from 'mongoose';
-import { StoreInterface } from './store.interface';
+import { JwtService } from '@nestjs/jwt';
+import { HttpService } from '@nestjs/axios';
+import { objStore } from './dto/store.dto';
 
+/*
+ A ver. Si lo que yo tengo que hacer es crear la store cuando se registra o se logea
+ tengo un problema ahí. 
+ Cómo sería la configuración allí?
+  
+ */
 @Injectable()
 export class StoreService {
   constructor(
     @InjectModel(Store.name)
     private readonly storeModel: Model<Store>,
+    private readonly httpService: HttpService,
+    private jwtService: JwtService,
   ) {}
 
-  async createStore(store: StoreInterface): Promise<Store> {
+  async createStore(store: objStore): Promise<Store> {
     return await this.storeModel.create(store);
   }
 
   async findOne(id: string): Promise<Store> {
     return await this.storeModel.findById(id);
+  }
+  async updateStore(store: objStore) {
+    return await this.storeModel.updateOne({ id: store.id }, store);
   }
 }
