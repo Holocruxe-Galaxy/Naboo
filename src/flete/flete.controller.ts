d3da2852dd69d2394669e8a520a8d0edaa7883dc
JaluGJ <gabriel.i.jalil@gmail.com>
2023-08-24 00:40:04 -0300

    refactor(flete); add correct controller and fix service

--- src/flete/flete.controller.ts
+++ src/flete/flete.controller.ts
@@ -1,69 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { FleteDto, ObjConsult } from './dto/flete.dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { FleteDto } from './dto/flete.dto';
 import { FleteService } from './flete.service';
 
 @Controller('flete')
 export class FleteController {
   constructor(private readonly fleteService: FleteService) {}
-  @Get()
-  getOrders() {
-    const data = [
-      {
-        orden_woo: '#85',
-        numero_seg: '85',
-        status_fex: '9',
-        modo: 'express',
-        fecha: '12/02/21',
-        pago: 'completado',
-      },
-      {
-        orden_woo: '#87',
-        numero_seg: '87',
-        status_fex: '16',
-        modo: 'programado',
-        fecha: '12/02/21',
-        pago: 'completado',
-      },
-      {
-        orden_woo: '#99',
-        numero_seg: '99',
-        status_fex: ' 14',
-        modo: 'express',
-        fecha: '12/02/21',
-        pago: 'pendiente',
-      },
-      {
-        orden_woo: '#85',
-        numero_seg: '85',
-        status_fex: ' 9',
-        modo: 'express',
-        fecha: '12/02/21',
-        pago: 'completado',
-      },
-      {
-        orden_woo: '#87',
-        numero_seg: '87',
-        status_fex: '2',
-        modo: 'programado',
-        fecha: '12/02/21',
-        pago: 'completado',
-      },
-      {
-        orden_woo: '#99',
-        numero_seg: '99',
-        status_fex: '0',
-        modo: 'express',
-        fecha: '12/02/21',
-        pago: 'pendiente',
-      },
-    ];
-
-    // Retornar un JSON en la respuesta
-    return data;
-  }
-  @Post('cotizar')
-  getPrice(@Body() objConsul: ObjConsult) {
-    return this.fleteService.consultarCosto(objConsul);
+  @Get(':access_key')
+  obtenerServiciosPorEmpresa(@Param('access_key') access_key: string) {
+    return this.fleteService.obtenerFletesDeEmpresaPorAccKey(access_key);
   }
   @Post()
   createFlete(@Body() fleteDto: FleteDto) {
